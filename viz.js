0fc5e2dda092c92165ab8d037247153516c5899d
lai282 <95855743+lai282@users.noreply.github.com>
2023-10-17 01:49:41 +1100

    JavaScript for loading visualisations on the HTML

---
+++ viz.js
@@ -0,0 +1,11 @@
+var vg_1 = "viz1.vg.json"; 
+vegaEmbed("#viz1_id", vg_1).then(function(result){}).catch(console.error);
+
+var vg_2 = "viz2.vg.json"; 
+vegaEmbed("#viz2_id", vg_2).then(function(result){}).catch(console.error);
+
+var vg_3 = "viz3.vg.json"; 
+vegaEmbed("#viz3_id", vg_3).then(function(result){}).catch(console.error);
+
+var vg_4 = "viz4.vg.json"; 
+vegaEmbed("#viz4_id", vg_4).then(function(result){}).catch(console.error);
